b3a5976896fc4c257f5ec3cddf8805a37725c037
SamuelMaldonado023 <Samuel.maldonado3@upr.edu>
2025-11-06 11:36:14 +0000

    modified:   Graph.hpp 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 using namespace std;
 
 int main() {
-  Graph G(10);
-  G.addEdge(0,9);
-  G.addEdge(4, 2);
-
-  G.print();
+    Graph G(5);
+    G.addEdge(0,1); G.addEdge(2,1); G.addEdge(3,4); G.addEdge(4,1);
+    G.print();
+    cout << G.numEdges() << " " << G.inDegree(1) << " " << (G.isInfluencer(1) ? "yes" : "no") << endl;
 }
