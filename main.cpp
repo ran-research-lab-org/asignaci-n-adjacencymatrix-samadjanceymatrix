7f5e08cef4389666785ad61a22211b66b786179f
SamuelMaldonado023 <Samuel.maldonado3@upr.edu>
2025-11-06 11:39:31 +0000

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,5 @@ int main() {
     Graph G(5);
     G.addEdge(0,1); G.addEdge(2,1); G.addEdge(3,4); G.addEdge(4,1);
     G.print();
-    cout << G.numEdges() << " " << G.inDegree(1) << " " << (G.isInfluencer(1) ? "yes" : "no") << endl;
+    cout << "\nThe number of edges is: " << G.numEdges() << "\nThe degree of 1 is: " << G.inDegree(1) << "\nIs 1 influencer?: " << (G.isInfluencer(1) ? "yes\n" : "no\n") << endl;
 }
